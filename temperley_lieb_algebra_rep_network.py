f9b6450565a9b4b8a25581d9888978e06c59d86b
mhajij <mustafa.hajij@gmail.com>
2020-12-27 15:15:20 -0800

    small fixes

--- temperley_lieb_algebra_rep_network.py
+++ temperley_lieb_algebra_rep_network.py
@@ -3,67 +3,67 @@ Created on Tue Oct 20 22:09:49 2020
 
 @author: Mustafa Hajij
 """
-
+import tensorflow as tf
 from tensorflow.keras import backend as K
-
 from tensorflow.keras.models import Model
-from tensorflow.keras.layers import Input, Concatenate,  Dense
 from tensorflow.keras.layers import Lambda
-import utilities as ut
+from tensorflow.keras.layers import Input, Concatenate
+from tensorflow.python.ops import math_ops
 
 import cus_layers as cl
 
-
-from tensorflow.python.ops import math_ops
-import tensorflow as tf
-
 import os
+
+
 os.system('cls')
 
 
 
 
 
-def TL_generator_ins_outs(Ugen, inputs, gen_position=2, total_dimension=3,input_dim=2 ):
+def tl_generator_ins_outs(Ugen, inputs, gen_position=2, total_dimension=3,input_dim=2 ):
     
     
     """
+    purpose:
+        
+        From the paper: the presentation of the TL algebra is given by :
+        TL_m=< U1,...,U_{m-1} | U_i*U_{i-1}*U_i=U_i, U_i*U_{i+1}*U_i=U_i ,U_i^2=\delta*U_i, UiUj = UjUi > 
+         
+        where 
+         
+        Ui=id^{i-1} X U X id^{m-i+1}, and U is a the two hooks shape curve in the TL generator Ui.
+         
+        hence it is enough to train Ugen for the entire rep of the TL algebra
+             
+        
+        this function defines a network f_{Ui}:R^n->R^n and for a subset of the domain (inputs), it returns f_{Ui}(input) 
+        
+
     
     input:
-        Ugen : network R^n->R^n, used to define the generator in the TL algebra
+    -----    
+        Ugen : Keras model R^n->R^n, used to define the generator in the TL algebra
         
-        Ui=id^{i-1} X Ugen X id^{m-i+1} 
+               Ui=id^{i-1} X Ugen X id^{m-i+1} 
+               
         inputs: input tensor
         
-        gen_position: this is the index "i" in Ui
+        gen_position: Integer,  this is the index "i" in Ui
         
-        total_dimension : this is the index m in TL_{m}
+        total_dimension : Integer, this is the index m in TL_{m}
         
-        input_dim: this is the dimension n on which the function Ugen : network R^n->R^n is defined. 
+        input_dim: Integer, this is the dimension n on which the function Ugen : network R^n->R^n is defined. 
         
-    returns :
+    returns:
+    --------
+        
+    
         f_{Ui}(inputs)
         
         where f_{Ui}=id^{i-1} X Ugen X id^{m-i+1}
         
-        
-        
 
-
-    purpose:
-        
-     From the paper: the presentation of the TL algebra is given by :
-     TL_m=< U1,...,U_{m-1} | U_i*U_{i-1}*U_i=U_i, U_i*U_{i+1}*U_i=U_i ,U_i^2=\delta*U_i, UiUj = UjUi > 
-     
-     where 
-     
-     Ui=id^{i-1} X U X id^{m-i+1}, and U is a the two hooks shape curve in the TL generator Ui.
-     
-     hence it is enough to train Ugen for the entire rep of the TL algebra
-         
-    
-    this function defines a network f_{Ui}:R^n->R^n and for a subset of the domain (inputs), it returns f_{Ui}(input) 
-    
     """
 
     con_list=[]
@@ -81,54 +81,54 @@ def TL_generator_ins_outs(Ugen, inputs, gen_position=2, total_dimension=3,input_
     
     con_list.append(out_gen)
         
-    for i in range(gen_position+1,total_dimension):
-        
+    for i in range(gen_position+1,total_dimension):        
         con_list.append(inputs[i])
-            
-    
+                
             
     con_final=Concatenate()(con_list)  
     
     outs=[]
         
-    for i in range(0,total_dimension):
-        
-        x_i=cl.slice_layerA(0,2*input_dim,-1,input_dim)(con_final)
+    for i in range(0,total_dimension):       
+        x_i=cl.SliceLayer(0,2*input_dim,-1,input_dim)(con_final)
         outs.append(x_i)
     
     return outs 
     
-    
-    
-    
-
-
-
-def TL_algebra_net(Ugen,delta=2,input_dim=2):
+def tl_algebra_net(Ugen,delta=2,input_dim=2):
         
     """
-    input:
-     
+
+    Purpose
+    --------
         
-     Ugen :  a network that is trained inside this network.
-     delta : a constant in the TL algebra.
-     input_dim : input dimension for Ugen.
-     
-    return : a model that outputs the both sides of the TL relations. These tensors are later used in the loss function for training.
     
-    purpose : training Ugen to satisfy the relations of the TL algebera.
-            (1) U_i*U_{i-1}*U_i=U_i
-            (2) U_i*U_{i+1}*U_i=U_i  
-            (3) U_i^2=\delta*U_i
-            
-     From the paper: the presentation of the TL algebra is given by :
-     TL_m=< U1,...,U_{m-1} | U_i*U_{i-1}*U_i=U_i, U_i*U_{i+1}*U_i=U_i ,U_i^2=\delta*U_i, UiUj = UjUi > 
+        training Ugen to satisfy the relations of the TL algebera.
+        (1) U_i*U_{i-1}*U_i=U_i
+        (2) U_i*U_{i+1}*U_i=U_i  
+        (3) U_i^2=\delta*U_i
+        
+         From the paper: the presentation of the TL algebra is given by :
+         TL_m=< U1,...,U_{m-1} | U_i*U_{i-1}*U_i=U_i, U_i*U_{i+1}*U_i=U_i ,U_i^2=\delta*U_i, UiUj = UjUi > 
+         
+         where 
+         
+         Ui=id^{i-1} X Ugen X id^{m-i+1}    
+         hence it is enough to train Ugen for the entire rep of the TL algebra
+
+    Parameters:
+    ---------- 
+        
+         Ugen :  a network that is trained inside this network.
+         delta : a constant in the TL algebra.
+         input_dim : input dimension for Ugen.
      
-     where 
-     
-     Ui=id^{i-1} X Ugen X id^{m-i+1}    
-     hence it is enough to train Ugen for the entire rep of the TL algebra
-     
+    Return
+    ------
+         A Keras model that outputs the both sides of the TL relations. These tensors are later used in the loss function for training.
+        
+    
+         
     """
     
     # define the input tensors
@@ -136,34 +136,29 @@ def TL_algebra_net(Ugen,delta=2,input_dim=2):
     input_tensor_2=Input(shape=(input_dim,))   
     input_tensor_3=Input(shape=(input_dim,)) 
     
-
     
     # relation : U_i*U_{i-1}*U_i=U_i
-    outs=TL_generator_ins_outs(Ugen,[input_tensor_1,input_tensor_2,input_tensor_3],gen_position=2, total_dimension=3,input_dim=input_dim)   
-    outs=TL_generator_ins_outs(Ugen,outs,gen_position=1, total_dimension=3,input_dim=input_dim)   
-    outs_side1_equation_1=TL_generator_ins_outs(Ugen,outs,gen_position=2, total_dimension=3,input_dim=input_dim)
-    
-    outs_side2_equation_1=TL_generator_ins_outs(Ugen,[input_tensor_1,input_tensor_2,input_tensor_3],gen_position=2, total_dimension=3,input_dim=input_dim)
-
+    #side1 
+    outs=tl_generator_ins_outs(Ugen,[input_tensor_1,input_tensor_2,input_tensor_3],gen_position=2, total_dimension=3,input_dim=input_dim)   
+    outs=tl_generator_ins_outs(Ugen,outs,gen_position=1, total_dimension=3,input_dim=input_dim)   
+    outs_side1_equation_1=tl_generator_ins_outs(Ugen,outs,gen_position=2, total_dimension=3,input_dim=input_dim)
+    #side2    
+    outs_side2_equation_1=tl_generator_ins_outs(Ugen,[input_tensor_1,input_tensor_2,input_tensor_3],gen_position=2, total_dimension=3,input_dim=input_dim)
 
 
     # relation : U_i*U_{i+1}*U_i=U_i  
-    outs=TL_generator_ins_outs(Ugen,[input_tensor_1,input_tensor_2,input_tensor_3],gen_position=1, total_dimension=3,input_dim=input_dim)   
-    outs=TL_generator_ins_outs(Ugen,outs,gen_position=2, total_dimension=3,input_dim=input_dim)   
-    outs_side1_equation_2=TL_generator_ins_outs(Ugen,outs,gen_position=1, total_dimension=3,input_dim=input_dim)
-
-    outs_side2_equation_2=TL_generator_ins_outs(Ugen,[input_tensor_1,input_tensor_2,input_tensor_3],gen_position=1, total_dimension=3,input_dim=input_dim)
-
-
-
-
-
-    
+    #side1 
+    outs=tl_generator_ins_outs(Ugen,[input_tensor_1,input_tensor_2,input_tensor_3],gen_position=1, total_dimension=3,input_dim=input_dim)   
+    outs=tl_generator_ins_outs(Ugen,outs,gen_position=2, total_dimension=3,input_dim=input_dim)   
+    outs_side1_equation_2=tl_generator_ins_outs(Ugen,outs,gen_position=1, total_dimension=3,input_dim=input_dim)
+    #side2
+    outs_side2_equation_2=tl_generator_ins_outs(Ugen,[input_tensor_1,input_tensor_2,input_tensor_3],gen_position=1, total_dimension=3,input_dim=input_dim)
+   
     # relation : U_{i}^2=\delta*U_{i} 
-    out_gen=Ugen(Concatenate()([input_tensor_1,input_tensor_2]) )
-
+    out_gen=Ugen(Concatenate()([input_tensor_1,input_tensor_2]) ) # requires two inputs
+    #side1 
     outs_side1_equation_3=Ugen(out_gen)
-       
+    #side2       
     outs_side2_equation_3 =Lambda(lambda x: x * delta) (  Ugen(Concatenate()([input_tensor_1,input_tensor_2])))
 
     
@@ -177,13 +172,19 @@ def TL_algebra_net(Ugen,delta=2,input_dim=2):
     return M
 
 
-def TL_loss_wrapper(input_dim=2):
+def tl_loss_wrapper(input_dim=2):
     
     """
-    input : input_dim, the dim of the U generator.
-    
-    purpose : loss for the TL algebra. When the loss is minimal, the TL relations are satisfied for the generator U.
-    
+    Purpose
+    -------
+        
+        loss for the TL algebra. When the loss is minimal, the TL relations are satisfied for the generator U.
+
+
+    Parameters
+    ----------    
+        input_dim, the dimension of the U generator for the TL algebra.
+        
     """
     
     def loss(y_true,y_pred):
@@ -206,16 +207,3 @@ def TL_loss_wrapper(input_dim=2):
        
         return A+B+C
     return loss
-
-
-
-
-
-
-
-
-
-
-
-
-
--- zxz_group_rep_network.py
+++ zxz_group_rep_network.py
@@ -5,13 +5,12 @@ Created on Tue Oct 20 22:09:49 2020
 @author: Mustafa Hajij
 """
 
-import numpy as np
 from tensorflow.keras import backend as K
-
 from tensorflow.keras.models import Model
 from tensorflow.keras.layers import Input, Concatenate
 from tensorflow.python.ops import math_ops
 import tensorflow as tf
+
 import os
 os.system('cls')
 
@@ -20,49 +19,69 @@ os.system('cls')
 def ZxZ_group_rep_net(a_op,b_op,input_shape=2):
     
     """
-    ZxZ=<a,b| ab=ba >
+    Purpose
+    -------
+        This is an axiluray network that is trained to force the relations on the input generators.
     
-    a_op : a network R^n->R^n represent the generator a in the presentation above.
-    b_op : a network R^n->R^n represent the generator b in the presentation above.
+        The network returns the sides of the relations of this algebraic structure.
+        
     
-    input_shape : dimension of the rep.
+    Parameters
+    ----------
     
-    purpose : this is an axiluray network that is trained to force the relations on the input generators.
+        a_op : a network R^n->R^n represent the generator a in the presentation above.
+        b_op : a network R^n->R^n represent the generator b in the presentation above.
     
-    the network returns the sides of the relations of this algebraic structure.
+        input_shape : dimension of the rep.
+    
+    Return
+    ------
+        Keras model, an auxiliary network used to fornce the relations on the generators of the ZxZ group.
+                     the output of this Keras model is the relations tensors which used inside a loss function 
+                     in order to make the these relations hold.
+      
+    
+    Note
+    ----
+        The group ZxZ has the presentation: ZxZ=<a,b| ab=ba >. In other words, it has two generators and one relation. 
     
     """
-        
-    
-    input_tensor_1=Input(shape=(input_shape,))
-        
+    input_tensor_1=Input(shape=(input_shape,))     
     # a*b side 
-    R_output=b_op(input_tensor_1) 
-    
+    R_output=b_op(input_tensor_1)  
     side_1_equation_1=a_op(R_output)
     
-    
     # b*a side
-    R_output=a_op(input_tensor_1)    
-    
+    R_output=a_op(input_tensor_1)     
     side_2_equation_1=b_op(R_output)
-
     
+    # Concatenate the two tensors and make the model
     output_tensor=Concatenate()([side_1_equation_1,side_2_equation_1])    
     M=Model(inputs=[input_tensor_1],outputs=output_tensor) 
     
     return M
 
-
 def ZxZ_group_rep_loss(input_dim=2):
+    
+    """
+    Purpose
+    -------
+        loss for the ZxZ group. When the loss is minimal, the relation a*b=b*a is satisfied for the generators a and b.
+    
+    Parameter
+    ---------
+    
+        input_dim, the dim of the a_op and b_op generators.
+        
+    """
     
     def loss(y_true,y_pred):
         
+        # split the output relation tensor of the model 
         equation_1_out=tf.slice(y_pred,[0,0],[-1,input_dim])
-        equation_2_out=tf.slice(y_pred,[0,input_dim],[-1,input_dim])
+        equation_2_out=tf.slice(y_pred,[0,input_dim],[-1,input_dim])   
         
-        A=K.mean(math_ops.square(equation_1_out - equation_2_out), axis=-1) 
-                    
+        A=K.mean(math_ops.square(equation_1_out - equation_2_out), axis=-1)                 
         return A
     
     return loss
